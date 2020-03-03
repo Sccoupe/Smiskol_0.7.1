cf31586121bac68b52fa8deabbb2cd3020ef1c45
Shane <shane@smiskol.com>
2020-03-02 21:28:58 -0600

    updates from comma

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -138,10 +138,10 @@ class Planner():
 
       desired_speed = speed + np.clip(distance/3.0, -speed*.1 - .5, speed*.1 + .5)
       speed_change = desired_speed - v_ego
-      desired_accel = np.clip((speed_change/2.0 + accel)/2.0, -ACCEL_LIMIT, ACCEL_LIMIT)
+      desired_accel = np.clip(speed_change/5.0 + accel, -ACCEL_LIMIT, ACCEL_LIMIT)
 
       self.a_model = desired_accel
-      self.v_model += self.a_model/FPS
+      self.v_model = desired_speed
     else:
       self.v_model = 50
       self.a_model = 10
